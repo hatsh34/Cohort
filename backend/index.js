2533d034fcc2d215ff355292a1332cf465969f63
harsh <harshsince2003@gmail.com>
2025-03-27 12:36:06 +0530

    file

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+// basic express boiler plate cp=ode
+
+const express = require('express');
+const app = express();
+const port = 3000;
+app.use(exppress.json());
+
+
+app.post("/todo", (req, res) =>{
+})
+
+app.get("/todos", (req,res) => {
+})
+
+app.put("/completed", (req,res) => {
+    
+})
