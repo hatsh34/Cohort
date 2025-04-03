a9ff0e1be05cf73d55af60d4723ebdd80bf359ec
harsh <harshsince2003@gmail.com>
2025-04-03 10:00:46 +0530

    finished

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const { createTodo, updateTodo } = require('./types');
 const { todo } = require('./db');
 const app = express();
+const cors = require("cors");
 const port = 3000;
+
+app.use(cors());
 app.use(express.json());
 
 
